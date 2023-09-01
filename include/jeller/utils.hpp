a0a2d3141d2e63d775aaa7f73e130b93f796e7c5
pawbar94 <paw.bar94@gmail.com>
2023-09-01 15:38:46 +0200

    Made default callback public

--- include/jeller/utils.hpp
+++ include/jeller/utils.hpp
@@ -4,14 +4,15 @@
 #include <string_view>
 
 #include "jeller/logging_level.hpp"
+#include "jeller/export_jeller_api.hpp"
 
 namespace jeller {
 
 class Utils
 {
 public:
+    PUBLIC_JELLER_API static void DefaultLoggingCallback(const std::string_view logger_name, const std::string_view logging_level, const std::string_view message);
     static std::string LoggingLevelToString(const LoggingLevel level);
-    static void DefaultLoggingCallback(const std::string_view logger_name, const std::string_view logging_level, const std::string_view message);
 
 private:
     static std::string GetDefaultLogLine(const std::string_view logger_name, const std::string_view logging_level, const std::string_view message);
